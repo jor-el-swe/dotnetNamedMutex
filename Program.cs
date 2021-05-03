501d23b2ecf4cc2126466d4661ad2983da349d3a
Joel Ring <9348156+jor-el-swe@users.noreply.github.com>
2021-05-03 11:19:07 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace dotnetNamedMutex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Application");
+            Mutex myMutex = new Mutex (true, "theVeryExclusiveMutex", out bool createdNew);
+            //check if mutex is availalbe
+            if (createdNew)
+            {
+                //then enter
+                Console.WriteLine("entering the very exciting program");
+                while (true); 
+            }
+            else
+            {
+                           
+                //else exit
+                Console.WriteLine("not enough resources to enter right now"); 
+            }
+
+        }
+    }
+}
